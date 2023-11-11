64451740326c6dc19d570e52ed786a0d0ec44cb1
Anh Khoa <anhkhoa5525@gmail.com>
2023-11-11 10:56:48 +0700

    Modified

--- src/main/java/com/cpkld/MovieTicketApplication.java
+++ src/main/java/com/cpkld/MovieTicketApplication.java
@@ -5,6 +5,8 @@ package com.cpkld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.context.ApplicationContext;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // import com.cpkld.model.entity.Role;
 // import com.cpkld.service.RoleService;
