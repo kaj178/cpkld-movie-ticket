ca4518f0970ad8723b93010872dba0106c1b4512
ISt <92091076+Strych-9@users.noreply.github.com>
2023-12-02 18:25:26 +0700

    Update postgres_datasource.sql

--- src/main/resources/postgres_datasource.sql
+++ src/main/resources/postgres_datasource.sql
@@ -609,9 +609,49 @@ INSERT INTO public."seat"("seat_name", "type", "room_id") VALUES
 ('Seat 56', 'VIP', 14);
 
 -- -- Thêm dữ liệu vào bảng "ticket"
--- INSERT INTO public."ticket" VALUES
---     ('ticket001', 'showtime001', 'seat001', 'status001'),
---     ('ticket002', 'showtime002', 'seat002', 'status001');
+INSERT INTO public."ticket"("showtime_id", "seat_id", "status_id") VALUES
+(1, 1, 1),
+(1, 2, 2),
+(1, 3, 1),
+(1, 4, 2),
+(2, 1, 1),
+(2, 2, 2),
+(2, 3, 1),
+(2, 4, 2),
+(3, 1, 1),
+(3, 2, 2),
+(3, 3, 1),
+(3, 4, 2),
+(4, 1, 1),
+(4, 2, 2),
+(4, 3, 1),
+(4, 4, 2),
+(5, 1, 1),
+(5, 2, 2),
+(5, 3, 1),
+(5, 4, 2),
+(6, 1, 1),
+(6, 2, 2),
+(6, 3, 1),
+(6, 4, 2),
+(7, 1, 1),
+(7, 2, 2),
+(7, 3, 1),
+(7, 4, 2),
+(8, 1, 1),
+(8, 2, 2),
+(8, 3, 1),
+(8, 4, 2),
+(9, 1, 1),
+(9, 2, 2),
+(9, 3, 1),
+(9, 4, 2),
+(10, 1, 1),
+(10, 2, 2),
+(10, 3, 1),
+(10, 4, 2);
+   
+
 
 -- -- Thêm dữ liệu vào bảng "menu"
 -- INSERT INTO public."menu" VALUES
